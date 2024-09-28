83b53474ab23b6b401b241a81db69fb5aac24d8f
Maxi Bartolozzi <mxbarto@gmail.com>
2024-09-28 08:37:01 -0300

    adding LoginRegister to the Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import LoginRegister from "../components/shared/LoginRegister"
 
 const Home = () => {
     return(
         <>
             <h3>Bienvenidos Muchaaachos</h3>
             <p>Acá va a haber información sobre el prode</p>
+            <LoginRegister />
         </>
     )
 }
