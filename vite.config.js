18cc718aef20c490b5def4fd2dac23da03d344ec
Maxi Bartolozzi <mxbarto@gmail.com>
2024-11-26 12:30:54 -0300

    add anv variables

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: import.meta.env.NODE_ENV === "production" ? "/prodefarina/" : "/"
+  base: import.meta.env.VITE_NODE_ENV === "production" ? "/prodefarina/" : "/"
 })
