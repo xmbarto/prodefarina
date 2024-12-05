4b340f10449efaaa3b99c2662df571c70787e976
Maxi Bartolozzi <mxbarto@gmail.com>
2024-12-05 11:22:17 -0300

    Update the Home content for testing purposes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,12 @@ const Home = () => {
 
     return(
         <>
-            <h2>Reglas del prode</h2>
-            <ul>
-                <li>Regla número 1</li>
-                <li>Regla número 2</li>
-            </ul>
+            <h2>Estamos probando cómo funciona esto</h2>
+            <ol>
+                <li>Intentá registrarte</li>
+                <li>Entrá a &quot;Jugar&quot;</li>
+                <li>Completá el prode enviame la tarjeta por Whatsapp</li>
+            </ol>
         </>
     )
 }
